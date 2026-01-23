bf6b0f3b7003b5ea7b3f1bbfc706619ec6e7da18
Michael2061 <michael.roehm@outlook.com>
2026-01-23 17:04:04 +0100

    change wallpaper_engine.sh

--- scripts/wallpaper_engine.sh
+++ scripts/wallpaper_engine.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# Pfade definieren
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
-# Zufälliges Bild wählen
+WAYBAR_STYLE="$HOME/.config/waybar/style.css"
+SWAYOSD_STYLE="$HOME/.config/swayosd/style.css"
+
+# 1. Zufälliges Bild aus deinen Wallpapern wählen
 WALLPAPER=$(ls $WALLPAPER_DIR/*.jpg | shuf -n 1)
 
-# 1. Bild setzen & Farben generieren
+# 2. Hintergrundbild setzen & Farben generieren
 swww img "$WALLPAPER" --transition-type wipe
 wal -i "$WALLPAPER"
 
-# 2. DER TRICK FÜR HYPRLOCK:
+# 3. DER TRICK FÜR HYPRLOCK:
 # Wir erstellen einen festen Link, den hyprlock immer finden kann
 ln -sf "$WALLPAPER" "$HOME/Pictures/Wallpapers/current_wallpaper.jpg"
 
-# 3. Pfade in den Styles fixen
-sed -i "s|__HOME__|$HOME|g" ~/.config/waybar/style.css
-sed -i "s|__HOME__|$HOME|g" ~/.config/swayosd/style.css
+# 4. Pfade in den Styles fixen (ersetzt __HOME__ durch deinen echten Pfad)
+sed -i "s|__HOME__|$HOME|g" "$WAYBAR_STYLE"
+sed -i "s|__HOME__|$HOME|g" "$SWAYOSD_STYLE"
 
-# 4. Reloads
-killall -SIGUSR2 waybar
-swayosd-client --reload-style
+# 5. Alles aktualisieren
+killall -SIGUSR2 waybar        # Waybar Farben neu laden
+swayosd-client --reload-style  # SwayOSD Farben neu laden
+
+echo "✅ Alles erledigt! Design angepasst an: $(basename "$WALLPAPER")"
